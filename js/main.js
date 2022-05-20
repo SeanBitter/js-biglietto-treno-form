62dd2ce27438a3542e8e48c609f055f0d26656b4
SeanBitter <paolodig@hotmail.it>
2022-05-20 16:15:52 +0200

    Complete the exercise

--- js/main.js
+++ js/main.js
@@ -20,3 +20,53 @@ maggiorenne
 over 65
 In base alla voce scelta dall’utente, fare i dovuti calcoli
 */
+
+const inputNome = document.querySelector("[name='nomeUtente']");
+const inputKm = document.querySelector("[name='kmUtente']");
+const inputEta = document.querySelector("[name='etaUtente']");
+const submitButton = document.getElementById("genera-biglietto");
+const cancelButton = document.getElementById("annulla-biglietto");
+
+const containerBiglietto = document.getElementById("container-biglietto");
+
+submitButton.addEventListener("click", function() {
+    
+    const kmDaPercorrere = parseInt(inputKm.value);
+    let prezzoAlKm = 0.21;
+
+    if (kmDaPercorrere >= 1200) {
+        alert("Inserisci un kilometraggio idoneo");
+    } else if (kmDaPercorrere <= 10) {
+        alert("Inserisci un kilometraggio idoneo");
+    }
+
+    if (inputEta.value === "minorenne") {
+        prezzo = prezzoAlKm / 100 * 80;
+        nomeOfferta = "Sconto Minorenni";
+    } else if (inputEta.value === "over65") {
+        prezzo = prezzoAlKm / 100 * 60;
+        nomeOfferta = "Sconto Over 65";
+    } else if (inputEta.value === "maggiorenne") {
+        prezzo = prezzoAlKm;
+        nomeOfferta = "Biglietto Standard"
+    }
+
+    document.getElementById("container-biglietto").classList.remove("d-none");
+    
+    document.getElementById("nome-utente").innerHTML = inputNome.value;
+    document.getElementById("offerta").innerHTML = nomeOfferta;
+    document.getElementById("carrozza").innerHTML = (Math.floor(Math.random() * 9) + 1);
+    document.getElementById("codice-cp").innerHTML = (Math.floor(Math.random() * 10000) + 1);
+    document.getElementById("costo-biglietto").innerHTML = (prezzo * kmDaPercorrere).toFixed(2);
+
+    }
+);
+
+cancelButton.addEventListener("click", function() {
+
+    inputNome.value = "";
+    inputKm.value = "";
+
+    document.getElementById("container-biglietto").classList.add("d-none");
+}
+);
